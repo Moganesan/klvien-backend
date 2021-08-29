ceeba9e34afbaaf8515ed27e874d40528e41f145
Moganesan M <ht.moganesan@gmail.com>
2021-08-29 08:06:10 +0530

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     method: ["GET", "POST"],
     credentials: true,
   })
