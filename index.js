5f12305d8bd3eea83408eceb07536045916276e1
Moganesan M <ht.moganesan@gmail.com>
2021-09-20 06:39:54 +0530

    commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(
     store: SessionDB,
     cookie: {
       sameSite: "none",
-      secure: false,
+      secure: true,
       name: "auth",
       maxAge: 60 * 60 * 24 * 5 * 1000,
     },
